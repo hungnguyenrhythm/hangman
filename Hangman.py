65756231767a3e1ccc1c698048cc4b5e8c8a4155
Hung Nguyen <hungcqnguyen@gmail.com>
2022-11-17 16:57:56 -0500

    We make the game become harder

--- Hangman.py
+++ Hangman.py
@@ -81,9 +81,6 @@ main()
 # import random module
 import random
 
-# import turtle module
-import turtle
-
 # Define function for finding the number of time the letter appears in the word to guess
 def num_of_letter(choice, word):
     if choice in word:
@@ -106,7 +103,7 @@ def display_progress(progress, word, guess, count):
     return result
 
 # Define function to tell body parts
-def body(attempt):
+def body(attempt, word):
     parts_of_body = ["Head", "Body", "Left Arm", "Right Arm", "Left Leg", "Right Leg"]
     print("You now have: ", end= " ")
     if attempt == 1:
@@ -115,11 +112,35 @@ def body(attempt):
         for i in range(attempt):    
             print(parts_of_body[i], end= " ")
         print()
+    if attempt >= 3:
+        print()
+        print(f"Your hint is: {dict_hint(word)}")
+        print()
 
 # Define function to draw body parts
 def draw_parts(attempt):
-    drawP_O_B = [draw_head, draw_body, draw_Larm, draw_Rarm, draw_Lleg, draw_Rleg]
-    drawP_O_B[attempt - 1]()
+    human = ["  0", "/", "|", "\\", " /", "\\"]
+    for index in range(attempt):
+        if index == 0 or index == 3 or index == 5:
+            print(human[index])
+        else:
+            print(human[index], end = " ")
+    print()
+    
+def dict_hint(word):
+    hints = {
+        "dog": "A stupid animal that serves as playthings for humans.",
+        "cat": "A stupid animal after reading the U.S. Constitution.",
+        "person": "I don't know, singular of people??????",
+        "iphone": "Overrated touch-screen phone with expensive prices and cheap components.",
+        "sophisticated": "use for describing someone who passes Calculus 2 with a 92 or above.",
+        "quetzacoatlus": "Extinct ancient flying dinosaurs.",
+        "anomalocaris": "Ancient rock lobsters.",
+        "smaragdine": "emerald green in color.",
+        "chiaroscurist": "use for implying an artist that watches too much european films."
+        }
+    hint = hints[word]
+    return hint
 
 # Define function to create the different levels of the game
 def select_level(level):
@@ -128,21 +149,23 @@ def select_level(level):
     if level == 2:
         levellist = ["person", "iphone"]
     if level == 3:
-        levellist = ["supercalafragilisticexpialadoshist"]
+        levellist = ["sophisticated", "quetzacoatlus", "anomalocaris", "smaragdine", "chiaroscurist"]
+    if level == 4:
+        levellist = ["pneumonoultramicroscopicsilicovolcanoconiosis", "antidisestablishmentarianism", "Pseudopseudohypoparathyroidism"]
     return levellist
 
 # Define the helper function to decide to go to the next level or ask to restart if you failed
 def option(level, fail):
     if fail == 1:
         choice = input("Do you want to restart(y) or do you want to quit(n): ")
-        if (choice != "y") and (choice != "n"):
+        while (choice != "y") and (choice != "n"):
             choice = input("Please try again: ")
         if choice == "y":
             main(level)
         elif choice == "n":
             print("GOODBYE")
     elif fail == 0:
-        if level < 4:
+        if level < 5:
             if level == 1:
                 print(f"On to level {level}")
                 main(level)
@@ -153,7 +176,43 @@ def option(level, fail):
                     main(level)
                 elif choice == "n":
                     print("GOODBYE")
-        elif level == 4:
+        elif level == 5:
             print("You Finished All The Levels CONGRADULATIONS!!!!!")
-            
 
+# define the main() function:
+def main(level):
+    word_lst = select_level(level)
+    word = random.choice(word_lst)
+    progress = "_" * len(word)
+    fail_attempts = 0
+    used_list = []
+    while fail_attempts < 6:
+        print(f"Your progress is {progress}.")
+        guess = input("Guess the character of the word(or press ENTER to quit): ")
+        while guess in used_list:
+            guess = input("The character has been used already, please try another one: ")
+        if (guess == "") or (guess == " "):
+            print("GAME OVER")
+            break
+        word_count = num_of_letter(guess, word)
+        if word_count > 0:
+            print(f"There's {word_count} letter {guess} in the word.")
+        else:
+            print(f"There's no letter {guess} in the word.")
+            fail_attempts += 1
+            body(fail_attempts, word)
+            draw_parts(fail_attempts)
+        progress = display_progress(progress, word, guess, word_count)
+        used_list.append(guess)
+        if progress == word:
+            break
+    if progress == word:
+        print(f"you got the word {word}")
+        print(F"CONGRADULATIONS YOU WIN LEVEL {level}")
+        level += 1
+        option(level, 0)
+    if fail_attempts == 6:
+        print("GAME OVER")
+        option(level, 1)
+
+option(1, 0)
